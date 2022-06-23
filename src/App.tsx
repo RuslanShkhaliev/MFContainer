6692c189267ac6b7946f22b2275c468b44de3845
Ruslan Shkhaliev <r.shkhaliev@pinkman.ru>
2022-06-23 11:11:17 +0300

    add react

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+/*
+ Created by Ruslan on 23.06.2022 (morehome@mail.ru)
+ */
+import React, { FC } from 'react'
+const App: FC = () => {
+
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
